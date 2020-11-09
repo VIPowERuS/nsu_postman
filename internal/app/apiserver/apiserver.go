08652d539e3f6c506f96f8276f8a220e218c0b09
Alexey Chechushkov <a.chechushkov1@g.nsu.ru>
2020-11-09 07:51:12 +0700

    Added some basic HTML templates and configuration for PostgreSQL database. A few main pages was added

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -2,8 +2,10 @@ package apiserver
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
+	"github.com/VIPowERuS/nsu_postman/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -13,6 +15,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New ...
@@ -31,6 +34,9 @@ func (s *APIServer) Start() error {
 	}
 
 	s.configureRouter()
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 	s.logger.Info("api server is started")
 	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
@@ -45,11 +51,50 @@ func (s *APIServer) configureLogger() error {
 }
 
 func (s *APIServer) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
+	s.router.HandleFunc("/", s.indexHandler())
+	s.router.HandleFunc("/writeAnnouncement", s.writeAnnouncementHandler())
+	s.router.HandleFunc("/SaveAnnouncement", s.saveAnnouncementHandler())
 }
 
-func (s *APIServer) handleHello() http.HandlerFunc {
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+	return nil
+}
+
+func (s *APIServer) indexHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello world")
+		t, err := template.ParseFiles("internal/templates/index.html", "internal/templates/header.html", "internal/templates/footer.html")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		t.ExecuteTemplate(w, "index", nil)
+	}
+}
+
+func (s *APIServer) writeAnnouncementHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("internal/templates/write.html", "internal/templates/header.html", "internal/templates/footer.html")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		t.ExecuteTemplate(w, "write", nil)
+	}
+}
+
+func (s *APIServer) saveAnnouncementHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.FormValue("id")
+		title := r.FormValue("title")
+		content := r.FormValue("content")
+		s.logger.Infoln(id, title, content)
+		http.Redirect(w, r, "/", 302)
 	}
 }
